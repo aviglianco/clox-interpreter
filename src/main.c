1982c71244a7dfb53bf7082839539100bd9979c5
aviglianco <aviglianco22@gmail.com>
2025-04-15 17:28:56 -0300

    Implement initial VM setup

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "chunk.h"
+#include "common.h"
+#include "vm.h"
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    // disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+
+    freeVM();
+    freeChunk(&chunk);
+    return 0;
+}
