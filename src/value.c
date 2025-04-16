ff3b20861236c7fffc5e0d3384d6df6618a231ce
aviglianco <aviglianco22@gmail.com>
2025-04-16 17:58:58 -0300

    Add support for booleans and nil

--- src/value.c
+++ src/value.c
@@ -3,6 +3,20 @@
 #include "memory.h"
 #include "value.h"
 
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_BOOL:
+            return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL:
+            return true;
+        case VAL_NUMBER:
+            return AS_NUMBER(a) == AS_NUMBER(b);
+        default:
+            return false;
+    }
+}
+
 void initValueArray(ValueArray *array) {
     array->capacity = 0;
     array->count = 0;
@@ -26,4 +40,16 @@ void freeValueArray(ValueArray *array) {
     initValueArray(array);
 }
 
-void printValue(Value value) { printf("%g", value); }
+void printValue(Value value) {
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:
+            printf("nil");
+            break;
+        case VAL_NUMBER:
+            printf("%g", AS_NUMBER(value));
+            break;
+    }
+}
